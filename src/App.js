13bd06f07e95e8be9bb6d8b9a018c729352c7699
RJ <a0970220839@gmail.com>
2023-09-30 16:45:04 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
 import logo from './logo.svg';
 import './App.css';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Cards from './cards'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <header>
+      <div
+        className='p-5 text-center bg-image'
+        style={{ backgroundImage: `url(${process.env.PUBLIC_URL}/images/galaxy.jpg)`, height: 300 , backgroundSize: 'cover',backgroundPosition: 'center'}}
+      >
+        <div className='mask' style={{ backgroundColor: 'rgba(0, 0, 0, 0.6)' }}>
+          <div className='d-flex justify-content-center align-items-center h-100'>
+            <div className='text-white'>
+              <h1 className='mb-3'>Planetary Tourism</h1>
+              <h4 className='mb-3'>To Infinity and Beyond!</h4>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+    
+    <div className="container">
+    <h1 className='pt-3 pb-3'>Browse the planet you wish to visit</h1>
+      <div className='pt-3 pb-3' style={{ display: 'flex', justifyContent: 'space-between' }}>
+        <Cards 
+          image = "./images/jupiter.jpg"
+          title="Jupiter"
+          description="Go to Jupiter"
+        />
+        <Cards 
+          image = "./images/mars.jpg"
+          title="Mars"
+          description="Go to Mars"
+        />
+        <Cards 
+          image = "./images/neptune.jpg"
+          title="Neptune"
+          description="Go to Neptune  "
+        />
+        <Cards 
+          image = "./images/saturn.jpg"
+          title="Saturn"
+          description="Go to Saturn"
+        />
+      </div>
+      <div className='pt-3 pb-3' style={{ display: 'flex', justifyContent: 'space-between' }}>
+        <Cards 
+          image = "./images/venus.jpg"
+          title="Venus"
+          description="Go to Venus"
+        />
+        <Cards 
+          image = "./images/mars.jpg"
+          title="Mars"
+          description="Go to Mars"
+        />
+        <Cards 
+          image = "./images/mars.jpg"
+          title="Mars"
+          description="Go to Mars"
+        />
+        <Cards 
+          image = "./images/mars.jpg"
+          title="Mars"
+          description="Go to Mars"
+        />
+      </div>
+    </div>
+    
     </div>
   );
 }
---
+++ src/cards.jsx
@@ -0,0 +1,30 @@
+import {
+    MDBCard,
+    MDBCardBody,
+    MDBCardTitle,
+    MDBCardText,
+    MDBCardImage,
+    MDBBtn,
+    MDBRipple
+  } from 'mdb-react-ui-kit';
+
+  function Cards ({image,title,description}){
+    return(
+        <MDBCard style={{ width: '300px', height: '450px' }}>
+      <MDBRipple rippleColor='light' rippleTag='div' className='bg-image hover-overlay'>
+        <MDBCardImage src={image} fluid alt='...' />
+        <a>
+          <div className='mask' style={{ backgroundColor: 'rgba(251, 251, 251, 0.15)' }}></div>
+        </a>
+      </MDBRipple>
+      <MDBCardBody>
+        <MDBCardTitle>{title}</MDBCardTitle>
+        <MDBCardText>
+          {description}
+        </MDBCardText>
+        <MDBBtn href='#'>Button</MDBBtn>
+      </MDBCardBody>
+    </MDBCard>
+    )
+  }
+  export default Cards;
