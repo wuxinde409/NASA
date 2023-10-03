b195807fb2cfe55f55bbb3c4712e93176118a480
RJ <a0970220839@gmail.com>
2023-10-03 16:59:14 +0800

    fix image src

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Cards from './cards'
+
 function App() {
   return (
     <div>
@@ -25,45 +25,45 @@ function App() {
     <h1 className='pt-3 pb-3'>Browse the planet you wish to visit</h1>
       <div className='pt-3 pb-3' style={{ display: 'flex', justifyContent: 'space-between' }}>
         <Cards 
-          image = "./images/Moon.jpg"
+          image = "https://images.unsplash.com/photo-1639382575211-e1959050f0fb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2080&q=80"
           title="Moon"
           description="Go to Moon"
         />
         <Cards 
-          image = "./images/venus.jpg"
+          image = "https://images.unsplash.com/photo-1639393455114-84df73f758cd?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2080&q=80"
           title="Venus"
           description="Go to Venus"
         />
         <Cards 
-          image = "./images/jupiter.jpg"
+          image = "https://images.unsplash.com/photo-1639477734993-44982980229e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2080&q=80"
           title="Jupiter"
           description="Go to Jupiter  "
         />
         <Cards 
-          image = "./images/mars.jpg"
+          image = "https://images.unsplash.com/photo-1639477735279-c36dda1f7ebb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2080&q=80"
           title="Mars"
           description="Go to Mars"
         />
       </div>
       <div className='pt-3 pb-3' style={{ display: 'flex', justifyContent: 'space-between' }}>
         <Cards 
-          image = "./images/neputune.jpg"
+          image = "https://images.unsplash.com/photo-1639477878754-4119b60fb02a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2080&q=80"
           title="Neputune"
           description="Go to Neputune"
         />
         <Cards 
-          image = "./images/uranus.jpg"
+          image = "https://images.unsplash.com/photo-1639548206689-1a5238f8d5bb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDZ8fHxlbnwwfHx8fHw%3D&auto=format&fit=crop&w=600&q=60"
           title="Uranus"
           description="Go to Uranus"
         />
         <Cards 
-          image = "./images/saturn.jpg"
+          image = "https://images.unsplash.com/photo-1639548206755-b20081a7cadc?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDV8fHxlbnwwfHx8fHw%3D&auto=format&fit=crop&w=600&q=60"
           title="Saturn"
           description="Go to Saturn"
         />
         <Cards 
-          image = "./images/mars.jpg"
-          title="Mars"
+          image = "https://images.unsplash.com/photo-1639548206689-1a5238f8d5bb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDZ8fHxlbnwwfHx8fHw%3D&auto=format&fit=crop&w=600&q=60"
+          title="Uranus"
           description="Go to Mars"
         />
       </div>
--- src/cards.jsx
+++ src/cards.jsx
@@ -12,7 +12,7 @@ import {
     return(
         <MDBCard style={{ width: '300px', height: '450px' }}>
       <MDBRipple rippleColor='light' rippleTag='div' className='bg-image hover-overlay'>
-        <MDBCardImage src={image} fluid alt='...' />
+        <MDBCardImage src= {image} fluid alt='loading'/>
         <a>
           <div className='mask' style={{ backgroundColor: 'rgba(251, 251, 251, 0.15)' }}></div>
         </a>
